eb9cd1a7d1f6285ca4a24ebb9daabc3aa541828f
UserLeeIsTired <leelearningzone@gmail.com>
2025-01-24 12:16:21 +0800

    should have used SHA-256 instead of MD5

--- SQL scripts/insert_user_trigger.sql
+++ SQL scripts/insert_user_trigger.sql
@@ -1,15 +1,19 @@
+-- Install extension first
+CREATE EXTENSION IF NOT EXISTS pgcrypto; 
+
 -- Create the trigger to set the salt and password_hash columns on insert
 CREATE OR REPLACE FUNCTION insert_tl_user_trigger() RETURNS TRIGGER AS $$
 BEGIN
     -- Generate a random key for the salt column
-    NEW.salt = md5(random()::text);
+    NEW.salt = encode(digest(random()::text, 'sha256'), 'hex');
 
     -- Concatenate the password with the salt and hash the combined value
-    NEW.password_hash = md5(NEW.password_hash || NEW.salt);
+    NEW.password_hash = encode(digest(NEW.password_hash || NEW.salt, 'sha256'), 'hex');
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
 
 -- Create the trigger on the tl_user table
 CREATE OR REPLACE TRIGGER insert_tl_user
